Fix: Fix normalization bug in LNOBlock. LNO convolution produces huge output after spectral convolution and need to be normalized via pytorch standard normalizer function. The related class was created for better function management and andding future features.

xno/layers/channel_mlp.py
@@ -59,7 +59,7 @@ class ChannelMLP(nn.Module):
             else:
                 self.fcs.append(nn.Conv1d(self.hidden_channels, self.hidden_channels, 1))
 
-    def forward(self, x):
+    def forward(self, x):        
         reshaped = False
         size = list(x.shape)
         if x.ndim > 3:  
@@ -80,7 +80,6 @@ class ChannelMLP(nn.Module):
         # same logic as above: .reshape() handles contiguous tensors as well
         if reshaped:
             x = x.reshape((size[0], self.out_channels, *size[2:]))
-
         return x
 
 

xno/layers/lno_block.py
@@ -97,6 +97,7 @@ class LNOBlocks(nn.Module):
         in_channels,
         out_channels,
         n_modes,
+        hidden_channels,
         # resolution_scaling_factor=None,
         n_layers=1,
         # max_n_modes=None,
@@ -145,6 +146,7 @@ class LNOBlocks(nn.Module):
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.n_layers = n_layers
+        self.hidden_channels = hidden_channels
         # self.stabilizer = stabilizer
         # self.rank = rank
         # self.factorization = factorization
@@ -167,6 +169,11 @@ class LNOBlocks(nn.Module):
         # else:
         #     self.non_linearity = non_linearity
         self.non_linearity = non_linearity
+        
+        """
+        Laplace Spectral Convolution specific normalizer.
+        """
+        self.nd_normalizer = NDNormalizer(num_channels=self.hidden_channels)
         
         self.convs = nn.ModuleList([
                 conv_module(
@@ -302,15 +309,14 @@ class LNOBlocks(nn.Module):
         #         x = ctanh(x)
         #     else:
         #         x = torch.tanh(x)
-
-        x_fno = self.convs[index](x, output_shape=output_shape)
+        x_lno = self.convs[index](x, output_shape=output_shape)
         #self.convs(x, index, output_shape=output_shape)
 
         # if self.norm is not None:
-        #     x_fno = self.norm[self.n_norms * index](x_fno)
+        #     x_lno = self.norm[self.n_norms * index](x_lno)
 
-        # x = x_fno + x_skip_fno
-        x = x_fno
+        # x = x_lno + x_skip_fno
+        x = self.nd_normalizer(x_lno)
 
         if (index < (self.n_layers - 1)):
             x = self.non_linearity(x)
@@ -403,3 +409,11 @@ class SubModule(nn.Module):
 
     def forward(self, x):
         return self.main_module.forward(x, self.indices)
+    
+class NDNormalizer(nn.Module):
+    def __init__(self, num_channels):
+        super(NDNormalizer, self).__init__()
+        self.norm = nn.GroupNorm(num_groups=1, num_channels=num_channels)
+
+    def forward(self, x):
+        return self.norm(x)

xno/layers/spectral_convolution_laplace.py
@@ -335,7 +335,6 @@ class SpectralConvLaplace2D(nn.Module):
                         term1)
         
         Pk=Hw  # for ode, Pk=-Hw; for 2d pde, Pk=Hw; for 3d pde, Pk=-Hw; 
-        import pdb; pdb.set_trace()
         output_residue1=torch.einsum("biox,oxikpq->bkox", 
                                      alpha, 
                                      Hw) 
@@ -350,7 +349,6 @@ class SpectralConvLaplace2D(nn.Module):
         x: torch.Tensor, 
         output_shape: Optional[Tuple[int]] = None
     ):
-        
         modes1, modes2 = self.n_modes
         H, W = x.shape[-2], x.shape[-1]
 
@@ -429,9 +427,8 @@ class SpectralConvLaplace2D(nn.Module):
                           term3)  # (b, o, H, W)
 
         x2 = torch.real(x2) / (x.size(-1) * x.size(-2))
-        
-        return x1 + x2  # Both are (b, o, H, W)
 
+        return x1 + x2  # Both are (b, o, H, W)
 
 class SpectralConvLaplace3D(nn.Module):
     def __init__(