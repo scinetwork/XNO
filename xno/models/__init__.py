1fe762c9db7431a4ce40b767d39fb30d3b9f4cda
Sina Pordanesh <sina.pordanesh@yahoo.com>
2024-12-31 13:06:38 -0700

    Pref: Remove dev phase import modules

--- xno/models/__init__.py
+++ xno/models/__init__.py
@@ -1,10 +1,10 @@
 from .xno import TXNO, TXNO1d, TXNO2d, TXNO3d
 from .fno import TFNO, TFNO1d, TFNO2d, TFNO3d
 from .fno import FNO, FNO1d, FNO2d, FNO3d
-from .hno import HNO, HNO1d, HNO2d, HNO3d
+# from .hno import HNO, HNO1d, HNO2d, HNO3d
 from .xno import XNO, XNO1d, XNO2d, XNO3d
-from .lno import LNO, LNO1d, LNO2d, LNO3d
-from .wno import WNO, WNO1d, WNO2d, WNO3d
+# from .lno import LNO, LNO1d, LNO2d, LNO3d
+# from .wno import WNO, WNO1d, WNO2d, WNO3d
 
 
 
--- xno/training/incremental.py
+++ xno/training/incremental.py
@@ -5,7 +5,8 @@ import torch
 from torch import nn
 
 from .trainer import Trainer
-from ..models import FNO, TFNO, HNO, XNO, LNO, WNO
+# from ..models import FNO, TFNO, HNO, XNO, LNO, WNO
+from ..models import FNO, TFNO, XNO
 from ..utils import compute_explained_variance
 
 class IncrementalFNOTrainer(Trainer):
@@ -42,7 +43,14 @@ class IncrementalFNOTrainer(Trainer):
                 incremental_loss_eps: float = 0.001,
                 ):
         self.model = model
-        assert (isinstance(model, FNO) or isinstance(self.model, TFNO) or isinstance(self.model, HNO) or isinstance(self.model, XNO) or isinstance(self.model, LNO) or isinstance(self.model, WNO)), f"Error: \
+        assert (
+            isinstance(model, FNO) or 
+            isinstance(self.model, TFNO) or 
+            isinstance(self.model, XNO) 
+            # isinstance(self.model, HNO) or 
+            # isinstance(self.model, LNO) or 
+            # isinstance(self.model, WNO)
+        ), f"Error: \
             IncrementalFNOTrainer is designed to work with FNO, TFNO, HNO, LNO, WNO and XNO, instead got\
             a model of type {model.__class__.__name__}"
             
